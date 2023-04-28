127a27e67f0142d651c0ee54c9d0e49d2767e0d5
Sammy Maldonado <sammyalejandro.m.p@gmail.com>
2023-04-28 13:52:39 -0400

    preparado para deployar

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import viewsRouter from './routes/views.router.js'
 import __dirname from './utils.js';
 
 const app = express();
-const server = app.listen(8080, () => console.log(`Listening on 8080`));
+const PORT = process.env.PORT || 8080;  //1) Le damos la libertad al puerto de escucharse en cualquier puerto.
+const server = app.listen(PORT, () => console.log(`Listening on 8080`));
 
 app.engine('handlebars',handlebars.engine());
 app.set('views',`${__dirname}/views`);
